6d398e66350ae78082e68fa9e0ee1302edd5ebfb
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-11-20 00:36:39 +0330

    feat : base of backend created

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,27 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerMiddleware } from '../../common/utils/logger';
+import { CustomConfigModule } from '../config/config.module';
+
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), '.env'),
+      ignoreEnvFile: false,
+    }),
+    CustomConfigModule,
+    AppModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
