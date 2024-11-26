5611a1d225bb482f5ff597766fd7d9fddf8ec238
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-11-26 18:23:34 +0330

    feat: user entity created

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { EntityNames } from "src/common/enums/entity-name.enum";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity(EntityNames.User)
+export class UserEntity {
+    @PrimaryGeneratedColumn('increment')
+    id:number;
+    @Column({nullable: true})
+    first_name : string
+    @Column({nullable: true})
+    last_name : string
+    @Column({unique: true})
+    mobile : string
+    @Column({nullable: true, unique : true})
+    email : string
+    @Column({unique : true})
+    invite_code : string
+    @Column({default: 0})
+    score : number
+    @Column({nullable: true})
+    agentId : number
+    @CreateDateColumn({type: "time with time zone"})
+    created_at : Date
+    @UpdateDateColumn({type: "time with time zone"})
+    updated_at : Date
+}
