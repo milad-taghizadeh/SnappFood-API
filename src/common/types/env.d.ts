61c1e39baffc1e04b47718b1725ffff0ecfc329e
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-11-28 11:45:23 +0330

    feat: s3 configured

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -7,5 +7,10 @@ namespace NodeJS {
     DB_USER: string;
     DB_PASSWORD: string;
     DB_HOST: string;
+    // S3
+    S3_ACCESS_KEY: string;
+    S3_SECRET_KEY: string;
+    S3_ENDPOINT: string;
+    S3_BUCKET_NAME: string;
   }
 }
